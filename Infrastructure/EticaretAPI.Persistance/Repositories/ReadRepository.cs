7dd723ec9ed76bd0c74994506f68de094250c9fa
sercan <sercanharputlu@gmail.com>
2022-08-16 12:22:53 +0300

    ef core tracking

--- Infrastructure/EticaretAPI.Persistance/Repositories/ReadRepository.cs
+++ Infrastructure/EticaretAPI.Persistance/Repositories/ReadRepository.cs
@@ -22,17 +22,60 @@ namespace EticaretAPI.Persistance.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
+        /*
+        public IQueryable<T> GetAll(bool tracking = true)
             => Table;
-        
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-            => Table.Where(method);
+        */
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        /*
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+            => Table.Where(method)
+        */
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        /*
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
             => await Table.FirstOrDefaultAsync(method);
 
-        public async Task<T> GetByIdAsync(string id)
+        */
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+
+        }
+        /*
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
             //=> await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
             => await Table.FindAsync(Guid.Parse(id));
+        */
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
+            
+
     }
 }
